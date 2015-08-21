b07db1ee434a127433b47cb29dfd0b6bbc38b9ad
Josh David Miller <josh@joshdmiller.com>
2015-08-20 20:35:57 -0700

    [test] add test for default Factory displayName

--- src/factory.spec.js
+++ src/factory.spec.js
@@ -3,13 +3,16 @@ import stampit from 'stampit';
 import Factory from './factory';
 
 test( 'Factory factory', t => {
-  t.plan( 3 );
+  t.plan( 4 );
 
   const ComponentB = Factory( 'ComponentB' );
   const ComponentA = Factory( 'ComponentA', [ ComponentB ] );
+  const ComponentC = Factory();
 
   t.ok( stampit.isStamp( ComponentB ), 'should return a stamp' );
   t.equals( ComponentB.displayName, 'ComponentB', 'should set the component displayName' );
   t.deepEquals( ComponentA.$inject, [ ComponentB ], 'should set the dependencies to $inject' );
+
+  t.equals( ComponentC.displayName, 'Factory', 'should have a default displayName of "Factory"' );
 });
 
